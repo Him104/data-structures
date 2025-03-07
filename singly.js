b421bc49148576aa5695c8bfc15ed087b2eb9222
him104 <himanshuchauhan696@gmail.com>
2025-03-08 01:31:20 +0530

    adding linked list

--- singly.js
+++ singly.js
@@ -11,31 +11,98 @@ class LinkedList {
     constructor() {
 
         this.head = null;
-
         this.length = 0
-
         
     }
 
 
     insertAtBeginning (data) {
-        let newNode = new Node(data)
-        newNode.next = this.head;
-        this.head = newNode;
+        let newNode = new Node(data) // Create a new node
+        newNode.next = this.head;       // Point new node to the current head
+        this.head = newNode; 
+        this.length++;           // Update head to the new node
     }
+
+
+    insertAtEnd (data) {
+        let newNode = new Node(data); // create a new Node
+       if (!this.head) {  // if list is empty, make it the head
+        this.head = newNode;
+        this.length++
+        return;
+        
+       }
+
+       let temp = this.head;
+       while (temp.next !== null) { //Traverse to the last node
+        temp = temp.next;
+       }
+       temp.next = newNode; // Link last node to new node
+    this.length++ 
+    }
+
+
+    deleteFromBeginning() {
+        if(!this.head) return; // if list is empty , do nothing
+        this.head = this.head.next; // Move head to the next node
+        this.length--;
+    }
+
+
+    deleteFromEnd() {
+        if (!this.head) return; // if list is empty , do nothing
+        
+        if(!this.head.next) { // if there is only one node, remove it
+
+            this.head = null;
+            return;
+
+        }
+
+        let temp = this.head
+        while (temp.next.next !== null) { // Traverse to the second last node
+
+            temp = temp.next
+            
+        }
+
+        temp.next = null; //Remove the last node by setting next to null
+        this.length--;
+    }
+
+
+
+
+    printList() {
+        let temp = this.head;
+        let result = "";
+        while (temp !== null) { // Traverse the list
+            result += temp.data + " -> ";
+            temp = temp.next;
+        }
+        result += "null";
+
+        console.log(result);
+    }
+
+    
 
 }
 
-    // insertAtEnd (data) {
-    //     let newNode = new Node(data); // create a new Node
-    //     newNode.next = this.head // Point new node to the current head
-    //     this.head = newNode; // update head to the new node
-    // }
+    
 
 
 let list = new LinkedList()
 
 list.insertAtBeginning(10)
 list.insertAtBeginning(20)
+list.insertAtEnd(30);
+list.printList()
+list.deleteFromBeginning()
+list.printList()
+list.deleteFromEnd()
 
-console.log(list)
+list.printList()
+
+
+console.log("Length : ",list.length)
